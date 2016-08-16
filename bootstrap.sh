daafbad4dcbe0c0d55e0a4769b52b79d7e2340be
Sergey Fedosimov <sfedosimov@gmail.com>
2016-08-17 00:13:41 +0300

    fix apache ssl conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -89,9 +89,7 @@ echo "
         <Directory /usr/lib/cgi-bin>
                 SSLOptions +StdEnvVars
         </Directory>
-        BrowserMatch \"MSIE [2-6]\" \
-                        nokeepalive ssl-unclean-shutdown \
-                        downgrade-1.0 force-response-1.0
+        BrowserMatch \"MSIE [2-6]\" nokeepalive ssl-unclean-shutdown downgrade-1.0 force-response-1.0
         BrowserMatch \"MSIE [17-9]\" ssl-unclean-shutdown
     </VirtualHost>
 </IfModule>
